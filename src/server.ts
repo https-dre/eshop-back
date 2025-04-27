2d40fcf8fd1d2d7e44551ff309f180556b101fd8
https-dre <diaso.andre@outlook.com>
2025-04-26 22:16:57 -0300

    refatora o servidor para registrar o middleware de multipart e reorganiza a estrutura do código

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,40 @@
 import fastify from "fastify";
-import fastifyCors from "@fastify/cors";
-import { validatorCompiler, serializerCompiler, jsonSchemaTransform } from "fastify-type-provider-zod";
-import { register_router } from "./router";
+import { validatorCompiler, serializerCompiler }
+    from "fastify-type-provider-zod";
+import { GetProducts } from "./controllers/get-products.controller";
+import { UploadImage } from "./controllers/upload-image.controller";
+import fastifyMultipart from "@fastify/multipart";
 
-const app = fastify();
-
-app.setValidatorCompiler(validatorCompiler);
-app.setSerializerCompiler(serializerCompiler);
-
-app.register(fastifyCors, {
-    origin: "*"
+const app = fastify({
+    ignoreTrailingSlash: true
 });
 
-app.register(register_router)
+const run = async () => {
+    await app.register(fastifyMultipart)
 
-if (!process.env.PORT) throw new Error("Missing PORT");
+    app.setValidatorCompiler(validatorCompiler);
+    app.setSerializerCompiler(serializerCompiler);
 
-app.listen({ port: parseInt(process.env.PORT)}).then(() => {
-    console.log("Server running in port: ", process.env.PORT)
-})
+    app.register(import('@fastify/cors'))
+
+    app.addHook('onError', (_, reply, error, done) => {
+        reply.status(500).send(error)
+    });
+
+    app.addHook('onRequest', (req, _, done) => {
+        console.log(req.originalUrl + " foi visitada!")
+        console.log(req.headers);
+        done();
+    })
+
+    await app.register(GetProducts);
+    await app.register(UploadImage);
+
+    if (!process.env.PORT) throw new Error("Missing PORT");
+
+    app.listen({ port: parseInt(process.env.PORT) }).then(() => {
+        console.log("Server running in port: ", process.env.PORT)
+    })
+}
+
+run();
