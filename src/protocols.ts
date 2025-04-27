b05cab789e032e7568fa8e94c922100e149aa23b
https-dre <diaso.andre@outlook.com>
2025-04-27 17:14:58 -0300

    adiciona interface ProductImageRepo para gerenciamento de imagens de produtos

--- src/protocols.ts
+++ src/protocols.ts
@@ -1,5 +1,9 @@
-import { Product } from "./models";
+import { Product, ProductImage } from "./models";
 
 export interface ProductRepo {
     get_products(limit: number, page?: number): Promise<Product[]>;
+}
+
+export interface ProductImageRepo {
+    save_image(data: ProductImage): Promise<void>;
 }
