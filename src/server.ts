9d402f535df9925f791463012bb4583aa7fcaa45
https-dre <diaso.andre@outlook.com>
2025-04-27 18:45:10 -0300

    refatora: organiza importações e registra fastifySwagger corretamente

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,20 @@
 import fastify from "fastify";
-import { validatorCompiler, serializerCompiler }
+import { validatorCompiler, serializerCompiler, jsonSchemaTransform }
     from "fastify-type-provider-zod";
+import fastifyMultipart from "@fastify/multipart";
+
 import { GetProducts } from "./controllers/get-products.controller";
 import { UploadImage } from "./controllers/upload-image.controller";
-import fastifyMultipart from "@fastify/multipart";
+import { GetImagesController } from "./controllers/get-images.controller";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 
 const app = fastify({
     ignoreTrailingSlash: true
 });
 
-const env = [process.env.PORT, process.env.SUPABASE_ID, 
-    process.env.SERVICE_ROLE, process.env.ANON_KEY, process.env.DATABASE_URL];
+const env = [process.env.PORT, process.env.SUPABASE_ID,
+process.env.SERVICE_ROLE, process.env.ANON_KEY, process.env.DATABASE_URL];
 
 for (const e of env) {
     if (!e) {
@@ -19,9 +23,26 @@ for (const e of env) {
 }
 
 const run = async () => {
-    await app.register(fastifyMultipart)
+    await app.register(fastifyMultipart);
 
-    app.setValidatorCompiler(validatorCompiler);
+    await app.register(fastifySwagger, {
+        swagger: {
+            consumes: ['application/json', 'multipart/form-data'],
+            produces: ['appplication/json'],
+            info: {
+                title: 'https-dre Shop',
+                description: '...',
+                version: '1.0.0'
+            },
+        },
+        transform: jsonSchemaTransform
+    })
+
+    await app.register(fastifySwaggerUi, {
+        routePrefix: '/docs'
+    });
+
+    app.setValidatorCompiler(validatorCompiler)
     app.setSerializerCompiler(serializerCompiler);
 
     app.register(import('@fastify/cors'))
@@ -33,6 +54,7 @@ const run = async () => {
 
     await app.register(GetProducts);
     await app.register(UploadImage);
+    await app.register(GetImagesController);
 
     if (!process.env.PORT) throw new Error("Missing PORT");
 
