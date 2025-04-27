cdb4df9ace37b70ffb5b4bfb4eeef3a62359a837
https-dre <diaso.andre@outlook.com>
2025-04-26 22:56:33 -0300

    refatora o seeder para separar a população de produtos e imagens, ajustando a estrutura e o uso de constantes

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -4,36 +4,80 @@ import { fakerPT_BR as faker } from "@faker-js/faker";
 import { Readable, Writable } from "stream";
 import { pipeline } from "stream/promises";
 
-const populate = async () => {
-    await sql`TRUNCATE TABLE products`;
+const BATCH_SIZE = 10_000;
+const BATCH_LIMIT = 20;
+
+const populate_products = async () => {
+    await sql`TRUNCATE TABLE products CASCADE`;
 
     console.time("products-to-insert")
-    const productsToInsert = Array.from({ length: 20 }, () => (
-        Array.from({ length: 10_000 }, () => ({
+    const productsToInsert = Array.from({ length: BATCH_LIMIT }, () => (
+        Array.from({ length: BATCH_SIZE }, () => ({
             id: randomUUID(),
             name: faker.commerce.productName(),
             description: faker.commerce.productDescription(),
             price_in_cents: faker.number.int({ min: 100, max: 10_000 }),
             created_at_utc: new Date().toUTCString()
-        }))
+        })
+        )
     ));
     console.timeEnd("products-to-insert")
 
     const reader = Readable.from(productsToInsert, { objectMode: true });
     const writer = new Writable({
         objectMode: true,
-        async write(chunk, encoding, callback) {
+        async write(chunk, _, callback) {
             await sql`INSERT INTO products ${sql(chunk)}`
             callback();
         }
     });
 
-    console.time("pipeline-time")
+    console.time("populando-products")
     await pipeline(
         reader,
         writer
     );
-    console.timeEnd("pipeline-time")
+    console.timeEnd("populando-products")
+}
+
+const populate_images = async () => {
+    console.time('images-to-insert');
+    await sql`TRUNCATE TABLE images CASCADE`;
+
+    const products = await sql`SELECT id FROM products`;
+    let current_p_index = 0;
+
+    let batch = Array.from({ length: BATCH_LIMIT }, () => {
+        let chunk = [];
+        for (let i = 0; i < BATCH_SIZE; i++) {
+            chunk.push({
+                id: randomUUID(),
+                name: 'sem_nome',
+                local: 'bucket/sem_nome.jpeg',
+                product_id: products[current_p_index].id
+            })
+            current_p_index++;
+        }
+        return chunk;
+    });
+    console.timeEnd('images-to-insert')
+
+
+    const readable = Readable.from(batch, { objectMode: true });
+    const writable = new Writable({
+        objectMode: true,
+        async write(chunk, _, callback) {
+            await sql`INSERT INTO images ${sql(chunk)}`
+            callback();
+        }
+    });
+
+    console.time('populando-images')
+    await pipeline(
+        readable,
+        writable
+    )
+    console.timeEnd('populando-images')
 }
 
 const seed = async () => {
@@ -52,12 +96,15 @@ const seed = async () => {
         CREATE TABLE IF NOT EXISTS images (
             id TEXT PRIMARY KEY,
             name VARCHAR(300) NOT NULL,
-            local TEXT NOT NULL
+            local TEXT NOT NULL,
+            product_id TEXT NOT NULL,
+            FOREIGN KEY (product_id) REFERENCES products(id)
         )
     `
 
-    if(process.env.TEST_DATABASE) {
-        await populate()
+    if (process.env.TEST_DATABASE) {
+        await populate_products();
+        await populate_images();
     }
 
     sql.end();
