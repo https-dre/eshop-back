aa49b87ed3e11b8eb49230348643b4e979c2e809
https-dre <diaso.andre@outlook.com>
2025-05-01 18:41:55 -0300

    adiciona nova rota 'POST /users' ao servidor

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import fastify from "fastify";
-import { validatorCompiler, serializerCompiler, jsonSchemaTransform }
-    from "fastify-type-provider-zod";
+import {
+	validatorCompiler,
+	serializerCompiler,
+	jsonSchemaTransform,
+} from "fastify-type-provider-zod";
 import fastifyMultipart from "@fastify/multipart";
 
 import { GetProducts } from "./controllers/get-products.controller";
@@ -9,56 +12,63 @@ import { GetImagesController } from "./controllers/get-images.controller";
 import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUi from "@fastify/swagger-ui";
 import { errorHandler } from "./error-handler";
+import { CreateUserController } from "./controllers/create-user.controller";
 
 const app = fastify({
-    ignoreTrailingSlash: true
+	ignoreTrailingSlash: true,
 });
 
-const env = [process.env.PORT, process.env.SUPABASE_ID,
-process.env.SERVICE_ROLE, process.env.ANON_KEY, process.env.DATABASE_URL];
+const env = [
+	process.env.PORT,
+	process.env.SUPABASE_ID,
+	process.env.SERVICE_ROLE,
+	process.env.ANON_KEY,
+	process.env.DATABASE_URL,
+];
 
 for (const e of env) {
-    if (!e) {
-        throw new Error('Missing env configs');
-    }
+	if (!e) {
+		throw new Error("Missing env configs");
+	}
 }
 
 const run = async () => {
-    await app.register(fastifyMultipart);
+	await app.register(fastifyMultipart);
 
-    await app.register(fastifySwagger, {
-        swagger: {
-            consumes: ['application/json', 'multipart/form-data'],
-            produces: ['appplication/json'],
-            info: {
-                title: 'E-commerce with https-dre',
-                description: '...',
-                version: '1.0.0'
-            },
-        },
-        transform: jsonSchemaTransform
-    })
+	await app.register(fastifySwagger, {
+		swagger: {
+			consumes: ["application/json", "multipart/form-data"],
+			produces: ["appplication/json"],
+			info: {
+				title: "E-commerce with https-dre",
+				description: "...",
+				version: "1.0.0",
+			},
+		},
+		transform: jsonSchemaTransform,
+	});
 
-    await app.register(fastifySwaggerUi, {
-        routePrefix: '/docs'
-    });
+	await app.register(fastifySwaggerUi, {
+		routePrefix: "/docs",
+	});
 
-    app.setValidatorCompiler(validatorCompiler)
-    app.setSerializerCompiler(serializerCompiler);
+	app.setValidatorCompiler(validatorCompiler);
+	app.setSerializerCompiler(serializerCompiler);
 
-    app.register(import('@fastify/cors'))
+	app.register(import("@fastify/cors"));
 
-    app.setErrorHandler(errorHandler);
+	app.setErrorHandler(errorHandler);
 
-    await app.register(GetProducts);
-    await app.register(UploadImage);
-    await app.register(GetImagesController);
+	await app.register(GetProducts);
+	await app.register(UploadImage);
+	await app.register(GetImagesController);
+	await app.register(CreateUserController);
 
-    if (!process.env.PORT) throw new Error("Missing PORT");
+	if (!process.env.PORT) throw new Error("Missing PORT");
 
-    app.listen({ port: parseInt(process.env.PORT) }).then(() => {
-        console.log("Server running in port: ", process.env.PORT)
-    })
-}
+	app.listen({ port: parseInt(process.env.PORT) }).then(() => {
+		console.log("Server running in port: ", process.env.PORT);
+	});
+};
 
 run();
